cbe247695f55deb5a78013805026d9c52fd5822a
Ryohei <ryohei.mmeve@gmail.com>
2021-12-11 16:35:13 +0900

    iam

--- environments/develop/iam/main.tf
+++ environments/develop/iam/main.tf
@@ -2,9 +2,9 @@
 
 variable "tenancy_ocid" {}
 variable "compartment_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
+#variable "user_ocid" {}
+#variable "fingerprint" {}
+#variable "private_key_path" {}
 variable "region" {}
 variable "homeregion" {}
 
@@ -19,9 +19,9 @@ terraform {
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  #user_ocid        = var.user_ocid
+  #fingerprint      = var.fingerprint
+  #private_key_path = var.private_key_path
   region           = var.homeregion
 }
 
@@ -39,94 +39,59 @@ provider "oci" {
  */
 
 module "iam_compartment" {
-  source                  = "oracle-terraform-modules/iam/oci//modules/iam-compartment"
+  source                  = "../../../modules/iam-compartment"
   # Pinning each module to a specific version is highly advisable. Please adjust and uncomment the line below
   # version               = "x.x.x"
   tenancy_ocid            = var.tenancy_ocid
   compartment_id          = var.tenancy_ocid # define the parent compartment. Creation at tenancy root if omitted
-  compartment_name        = "tf_example_compartment"
-  compartment_description = "compartment at root level created - terraformed"
-  compartment_create      = true # if false, a data source with a matching name is created instead
-  enable_delete           = true # if false, on `terraform destroy`, compartment is deleted from the terraform state but not from oci 
-}
-
-module "iam_subcompartment1" {
-  source                  = "oracle-terraform-modules/iam/oci//modules/iam-compartment"
-  # Pinning each module to a specific version is highly advisable. Please adjust and uncomment the line below
-  # version               = "x.x.x"
-  tenancy_ocid            = var.tenancy_ocid
-  compartment_id          = module.iam_compartment.compartment_id # define the parent compartment. Here we make reference to the previous module
-  compartment_name        = "tf_example_subcompartment1"
-  compartment_description = "subcompartment created below tf_example_compartment - terraformed"
-  compartment_create      = true # if false, a data source with a matching name is created instead
-  enable_delete           = true # if false, on `terraform destroy`, compartment is deleted from the terraform state but not from oci 
-}
-
-module "iam_subcompartment2" {
-  source                  = "oracle-terraform-modules/iam/oci//modules/iam-compartment"
-  # Pinning each module to a specific version is highly advisable. Please adjust and uncomment the line below
-  # version               = "x.x.x"
-  tenancy_ocid            = var.tenancy_ocid
-  compartment_id          = module.iam_compartment.compartment_id # define the parent compartment. Here we make reference to the previous module
-  compartment_name        = "tf_example_subcompartment2"
-  compartment_description = "subcompartment created below tf_example_compartment - terraformed"
+  compartment_name        = "dev"
+  compartment_description = "This compartment is for development."
   compartment_create      = true # if false, a data source with a matching name is created instead
   enable_delete           = true # if false, on `terraform destroy`, compartment is deleted from the terraform state but not from oci 
 }
 
 module "iam_users" {
-  source       = "oracle-terraform-modules/iam/oci//modules/iam-user"
+  source       = "../../../modules/iam-user"
   # Pinning each module to a specific version is highly advisable. Please adjust and uncomment the line below
   # version       = "x.x.x"
   tenancy_ocid = var.tenancy_ocid
   users = [
-    { # user1
-      name        = "tf_example_user1@example.com"
-      description = "user1 created by terraform"
+    {
+      name        = "ryohei"
+      description = "user for ryohei"
       email       = null
     },
-    { # user2
-      name        = "tf_example_user2@example.com"
-      description = "user2 created by terraform"
-      email       = "tf_example_user2@example.com"
-    },
-    { # user3
-      name        = "tf_example_user3@example.com"
-      description = "user3 created by terraform"
-      email       = "tf_example_user3@example.com"
-    }, # add more users below if needed
   ]
 }
 
 module "iam_group" {
-  source                = "oracle-terraform-modules/iam/oci//modules/iam-group"
-  version               = "2.0.0"
+  source                = "../../../modules/iam-group"
+  #version               = "2.0.0"
   tenancy_ocid          = var.tenancy_ocid
-  group_name            = "tf_example_group"
-  group_description     = "an example group - terraformed"
+  group_name            = "dev_admin"
+  group_description     = "Admin group for development compartment."
   user_ids              = [element(module.iam_users.user_id, 0), element(module.iam_users.user_id, 1), element(module.iam_users.user_id, 2)] # a list of user ocids
-  policy_name           = "tf-example-policy"
+  policy_name           = "dev_admin_policy"
   policy_compartment_id = module.iam_compartment.compartment_id
-  policy_description    = "policy created by terraform aaaaaaaaaah"
+  policy_description    = "Admin policy for development compartment."
   policy_statements = [
-    "Allow group ${module.iam_group.group_name} to read instances in compartment tf_example_compartment",
-    "Allow group ${module.iam_group.group_name} to inspect instances in compartment tf_example_compartment",
+    "Allow group ${module.iam_group.group_name} to manage instances in compartment ${module.iam_compartment.compartment_name}",
   ]
 }
 
-module "iam_dynamic_group" {
-  source                    = "oracle-terraform-modules/iam/oci//modules/iam-dyanmic-group"
-  # Pinning each module to a specific version is highly advisable. Please adjust and uncomment the line below
-  # version               = "x.x.x"
-  tenancy_ocid              = var.tenancy_ocid
-  dynamic_group_name        = "tf_example_dynamic_group"
-  dynamic_group_description = "dynamic group created by terraform"
-  matching_rule             = "instance.compartment.id = '${module.iam_compartment.compartment_id}'"
-  policy_compartment_id     = module.iam_compartment.compartment_id
-  policy_name               = "tf-example-dynamic-policy"
-  policy_description        = "dynamic policy created by terraform"
-  policy_statements = [
-    "Allow dynamic-group ${module.iam_dynamic_group.dynamic_group_name} to read instances in compartment tf_example_compartment",
-    "Allow dynamic-group ${module.iam_dynamic_group.dynamic_group_name} to inspect instances in compartment tf_example_compartment",
-  ]
-}
+#module "iam_dynamic_group" {
+#  source                    = "oracle-terraform-modules/iam/oci//modules/iam-dyanmic-group"
+#  # Pinning each module to a specific version is highly advisable. Please adjust and uncomment the line below
+#  # version               = "x.x.x"
+#  tenancy_ocid              = var.tenancy_ocid
+#  dynamic_group_name        = "tf_example_dynamic_group"
+#  dynamic_group_description = "dynamic group created by terraform"
+#  matching_rule             = "instance.compartment.id = '${module.iam_compartment.compartment_id}'"
+#  policy_compartment_id     = module.iam_compartment.compartment_id
+#  policy_name               = "tf-example-dynamic-policy"
+#  policy_description        = "dynamic policy created by terraform"
+#  policy_statements = [
+#    "Allow dynamic-group ${module.iam_dynamic_group.dynamic_group_name} to read instances in compartment tf_example_compartment",
+#    "Allow dynamic-group ${module.iam_dynamic_group.dynamic_group_name} to inspect instances in compartment tf_example_compartment",
+#  ]
+#}
