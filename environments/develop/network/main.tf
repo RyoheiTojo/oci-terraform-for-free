4ac77d7dd0e3acb383f5cc2a942bfb1b5138d8d0
Ryohei <ryohei.mmeve@gmail.com>
2021-12-12 10:22:14 +0900

    add vcn

---
+++ environments/develop/network/main.tf
@@ -0,0 +1,35 @@
+variable "compartment_name" {}
+variable "tenancy_ocid" {}
+variable "homeregion" {}
+variable "vcn_cidr" {}
+variable "label_prefix" {}
+variable "vcn_dns_label" {}
+variable "enable_ipv6" {}
+variable "freeform_tags" {}
+variable "vcn_name" {}
+
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    oci = {
+      version = ">= 3.27, < 4.0" // force downloading oci-provider compatible with terraform v0.12
+    }
+  }
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  region           = var.homeregion
+}
+
+module "vcn" {
+  source           = "../../../modules/vcn"
+  tenancy_ocid     = var.tenancy_ocid
+  compartment_name = var.compartment_name
+  vcn_cidr         = var.vcn_cidr
+  vcn_dns_label    = var.vcn_dns_label
+  label_prefix     = var.label_prefix
+  enable_ipv6      = var.enable_ipv6
+  freeform_tags    = var.freeform_tags
+  vcn_name         = var.vcn_name
+}
---
+++ modules/vcn/variables.tf
@@ -0,0 +1,49 @@
+variable "tenancy_ocid" {
+  type = string
+  description = "The name to root compartment ocid."
+  default     = null
+}
+
+variable "compartment_name" {
+  type = string
+  description = "The name to pick up filtering compartment for vcn config."
+  default     = null
+}
+
+variable "vcn_cidr" {
+  description = "The IPv4 CIDR block the VCN will use."
+  default     = "10.0.0.0/16"
+  type        = string
+}
+
+variable "vcn_dns_label" {
+  description = "A DNS label for the VCN, used in conjunction with the VNIC's hostname and subnet's DNS label to form a fully qualified domain name (FQDN) for each VNIC within this subnet"
+  type        = string
+  default     = "vcnmodule"
+}
+
+variable "label_prefix" {
+  description = "a string that will be prepended to all resources"
+  type        = string
+  default     = "none"
+}
+
+variable "freeform_tags" {
+  description = "simple key-value pairs to tag the created resources using freeform OCI Free-form tags."
+  type        = map(any)
+  default = {
+    terraformed = "Please do not edit manually"
+  }
+}
+
+variable "enable_ipv6" {
+  description = "Whether IPv6 is enabled for the VCN. If enabled, Oracle will assign the VCN a IPv6 /56 CIDR block."
+  type        = bool
+  default     = false
+}
+
+variable "vcn_name" {
+  description = "user-friendly name of to use for the vcn to be appended to the label_prefix"
+  type        = string
+  default     = "vcn"
+}
