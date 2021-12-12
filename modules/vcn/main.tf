b1bf4bda49c82a885a8583422cfec3891df604e6
Ryohei <ryohei.mmeve@gmail.com>
2021-12-12 11:15:14 +0900

    fix

--- modules/vcn/main.tf
+++ modules/vcn/main.tf
@@ -4,13 +4,13 @@ data "oci_identity_compartments" "this" {
 
   filter {
     name   = "name"
-    values = [var.compartment_name]
+    values = [var.compartment_name] # TODO: there is a posibility to match several candidates.
   }
 }
 
 resource "oci_core_vcn" "vcn" {
   cidr_block     = var.vcn_cidr
-  compartment_id = data.oci_identity_compartments.this.compartment_id
+  compartment_id = data.oci_identity_compartments.this.compartments[0].id
   display_name   = var.label_prefix == "none" ? var.vcn_name : "${var.label_prefix}-${var.vcn_name}"
   dns_label      = var.vcn_dns_label
   is_ipv6enabled = var.enable_ipv6
