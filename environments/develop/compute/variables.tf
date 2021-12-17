09f3ea65db3fc751b19ebbf4a146af088386a113
Ryohei <ryohei.mmeve@gmail.com>
2021-12-18 02:59:10 +0900

    fix

---
+++ environments/develop/compute/variables.tf
@@ -0,0 +1,46 @@
+variable "tenancy_ocid" {}
+variable "homeregion" {}
+variable "ssh_authorized_keys" {}
+variable "user_data" {}
+
+variable "vcn_name" {
+  type = string
+  description = "Target vcn name"
+  default = null
+}
+
+variable "compartment_name" {
+  type = string
+  description = "Target compartment name"
+  default = null
+}
+
+variable "ad_index" {
+  type        = number
+  description = "Availability domain index"
+  default     = 0 # You can choose between 0 and 2.
+}
+
+variable "computes" {
+  type = map(object({
+    assign_public_ip        = bool,
+    fd_index                = number,
+    shape                   = string,
+    network_security_groups = list(string),
+    subnet_name             = string,
+    source_type             = string,
+    source                  = string,
+  }))
+  description = "Computes list"
+  default = {
+    default-compute = {
+      assign_public_ip        = false
+      fd_index                = null
+      network_security_groups = []
+      shape                   = null
+      source                  = null
+      source_type             = null
+      subnet_name             = null
+    }
+  }
+}
